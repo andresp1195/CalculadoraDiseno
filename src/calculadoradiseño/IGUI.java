3dc4af4417e1c419ade119791f3b049d71e36d5f
Mauricio Gamboa <Mauricio Gamboa@172.19.14.199>
2015-04-27 15:35:44 -0600

    Segundo Commit

---
+++ src/calculadoradiseño/IGUI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoradiseño;
+
+/**
+ *
+ * @author Mauricio Gamboa
+ */
+public interface IGUI {
+    
+}
