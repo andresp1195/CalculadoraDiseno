bf080e10f99de083fbe383686274597f51344a88
Mauricio Gamboa <Mauricio Gamboa@172.19.13.198>
2015-05-05 15:26:09 -0600

    Cambios sobre Modo Caracter y Operaciones Sistema

--- src/VistaModoCaracter/OperacionesSistema.java
+++ src/VistaModoCaracter/OperacionesSistema.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package VistaModoCaracter;
 
 import java.util.LinkedList;
@@ -15,11 +10,13 @@ public class OperacionesSistema {
     //public List<Integer> lista_operaciones; 
     public String Nombre; 
     public int ID; 
+    public String Tipo;
 
 
-public OperacionesSistema(String nombre,int ID){
+public OperacionesSistema(String nombre,int ID, String tipo){
     this.Nombre=nombre; 
     this.ID=ID; 
+    this.Tipo= tipo; 
 }
 
 }
